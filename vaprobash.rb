110ae6106ef64f3495e624ad35dfe1755d378a7b
YongHun Byun <yonghunbyun@gmail.com>
2015-06-20 21:05:12 +0900

    Add vaprobash.rb

---
+++ vaprobash.rb
@@ -0,0 +1,192 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Config Github Settings
+github_username = "fideloper"
+github_repo     = "Vaprobash"
+github_branch   = "1.4.0"
+github_url      = "https://raw.githubusercontent.com/#{github_username}/#{github_repo}/#{github_branch}"
+
+
+# Database Configuration
+pgsql_root_password   = "root"   # We'll assume user "root"
+mongo_enable_remote   = "false"  # remote access enabled when true
+
+# Languages and Packages
+ruby_version          = "latest" # Choose what ruby version should be installed (will also be the default version)
+ruby_gems             = [        # List any Ruby Gems that you want to install
+  #"jekyll",
+  #"sass",
+  #"compass",
+]
+
+# RabbitMQ settings
+rabbitmq_user = "user"
+rabbitmq_password = "password"
+
+sphinxsearch_version  = "rel22" # rel20, rel21, rel22, beta, daily, stable
+
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  ####
+  # Base Items
+  ##########
+
+  # Provision Base Packages
+  # config.vm.provision "shell", path: "#{github_url}/scripts/base.sh", args: [github_url, server_swap, server_timezone]
+
+  # optimize base box
+  # config.vm.provision "shell", path: "#{github_url}/scripts/base_box_optimizations.sh", privileged: true
+
+  # Provision PHP
+  # config.vm.provision "shell", path: "#{github_url}/scripts/php.sh", args: [php_timezone, hhvm, php_version]
+
+  # Enable MSSQL for PHP
+  # config.vm.provision "shell", path: "#{github_url}/scripts/mssql.sh"
+
+  # Provision Vim
+  # config.vm.provision "shell", path: "#{github_url}/scripts/vim.sh", args: github_url
+
+  # Provision Docker
+  # config.vm.provision "shell", path: "#{github_url}/scripts/docker.sh", args: "permissions"
+
+  ####
+  # Web Servers
+  ##########
+
+  # Provision Apache Base
+  # config.vm.provision "shell", path: "#{github_url}/scripts/apache.sh", args: [server_ip, public_folder, hostname, github_url]
+
+  # Provision Nginx Base
+  # config.vm.provision "shell", path: "#{github_url}/scripts/nginx.sh", args: [server_ip, public_folder, hostname, github_url]
+
+
+  ####
+  # Databases
+  ##########
+
+  # Provision MySQL
+  # config.vm.provision "shell", path: "#{github_url}/scripts/mysql.sh", args: [mysql_root_password, mysql_version, mysql_enable_remote]
+
+  # Provision PostgreSQL
+  # config.vm.provision "shell", path: "#{github_url}/scripts/pgsql.sh", args: pgsql_root_password
+
+  # Provision SQLite
+  # config.vm.provision "shell", path: "#{github_url}/scripts/sqlite.sh"
+
+  # Provision RethinkDB
+  # config.vm.provision "shell", path: "#{github_url}/scripts/rethinkdb.sh", args: pgsql_root_password
+
+  # Provision Couchbase
+  # config.vm.provision "shell", path: "#{github_url}/scripts/couchbase.sh"
+
+  # Provision CouchDB
+  # config.vm.provision "shell", path: "#{github_url}/scripts/couchdb.sh"
+
+  # Provision MongoDB
+  # config.vm.provision "shell", path: "#{github_url}/scripts/mongodb.sh", args: mongo_enable_remote
+
+  # Provision MariaDB
+  # config.vm.provision "shell", path: "#{github_url}/scripts/mariadb.sh", args: [mysql_root_password, mysql_enable_remote]
+
+  ####
+  # Search Servers
+  ##########
+
+  # Install Elasticsearch
+  # config.vm.provision "shell", path: "#{github_url}/scripts/elasticsearch.sh"
+
+  # Install SphinxSearch
+  # config.vm.provision "shell", path: "#{github_url}/scripts/sphinxsearch.sh", args: [sphinxsearch_version]
+
+  ####
+  # Search Server Administration (web-based)
+  ##########
+
+  # Install ElasticHQ
+  # Admin for: Elasticsearch
+  # Works on: Apache2, Nginx
+  # config.vm.provision "shell", path: "#{github_url}/scripts/elastichq.sh"
+
+
+  ####
+  # In-Memory Stores
+  ##########
+
+  # Install Memcached
+  # config.vm.provision "shell", path: "#{github_url}/scripts/memcached.sh"
+
+  # Provision Redis (without journaling and persistence)
+  # config.vm.provision "shell", path: "#{github_url}/scripts/redis.sh"
+
+  # Provision Redis (with journaling and persistence)
+  # config.vm.provision "shell", path: "#{github_url}/scripts/redis.sh", args: "persistent"
+  # NOTE: It is safe to run this to add persistence even if originally provisioned without persistence
+
+
+  ####
+  # Utility (queue)
+  ##########
+
+  # Install Beanstalkd
+  # config.vm.provision "shell", path: "#{github_url}/scripts/beanstalkd.sh"
+
+  # Install Heroku Toolbelt
+  # config.vm.provision "shell", path: "https://toolbelt.heroku.com/install-ubuntu.sh"
+
+  # Install Supervisord
+  # config.vm.provision "shell", path: "#{github_url}/scripts/supervisord.sh"
+
+  # Install ØMQ
+  # config.vm.provision "shell", path: "#{github_url}/scripts/zeromq.sh"
+
+  # Install RabbitMQ
+  # config.vm.provision "shell", path: "#{github_url}/scripts/rabbitmq.sh", args: [rabbitmq_user, rabbitmq_password]
+
+  ####
+  # Additional Languages
+  ##########
+
+  # Install Nodejs
+  # config.vm.provision "shell", path: "#{github_url}/scripts/nodejs.sh", privileged: false, args: nodejs_packages.unshift(nodejs_version, github_url)
+
+  # Install Ruby Version Manager (RVM)
+  # config.vm.provision "shell", path: "#{github_url}/scripts/rvm.sh", privileged: false, args: ruby_gems.unshift(ruby_version)
+
+  ####
+  # Frameworks and Tooling
+  ##########
+
+  # Provision Composer
+  # config.vm.provision "shell", path: "#{github_url}/scripts/composer.sh", privileged: false, args: composer_packages.join(" ")
+
+  # Provision Laravel
+  # config.vm.provision "shell", path: "#{github_url}/scripts/laravel.sh", privileged: false, args: [server_ip, laravel_root_folder, public_folder, laravel_version]
+
+  # Provision Symfony
+  # config.vm.provision "shell", path: "#{github_url}/scripts/symfony.sh", privileged: false, args: [server_ip, symfony_root_folder, public_folder]
+
+  # Install Screen
+  # config.vm.provision "shell", path: "#{github_url}/scripts/screen.sh"
+
+  # Install Mailcatcher
+  # config.vm.provision "shell", path: "#{github_url}/scripts/mailcatcher.sh"
+
+  # Install git-ftp
+  # config.vm.provision "shell", path: "#{github_url}/scripts/git-ftp.sh", privileged: false
+
+  # Install Ansible
+  # config.vm.provision "shell", path: "#{github_url}/scripts/ansible.sh"
+
+  # Install Android
+  # config.vm.provision "shell", path: "#{github_url}/scripts/android.sh"
+
+  ####
+  # Local Scripts
+  # Any local scripts you may want to run post-provisioning.
+  # Add these to the same directory as the Vagrantfile.
+  ##########
+  # config.vm.provision "shell", path: "./local-script.sh"
+
+end
